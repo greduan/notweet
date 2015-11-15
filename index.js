52f016408be6989484b31cf81a257676e27494ab
greduan <me@greduan.com>
2015-11-14 18:33:03 -0600

    Make more modular, TweetsContainer

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 'use strict'
 
 var path = require('path'),
+  config = require(path.join(__dirname, 'config.js')),
   Twit = require('twit'),
-  blessed = require('blessed'),
-  config = require(path.join(__dirname, 'config.js'))
+  T = new Twit(config)
 
-var T = new Twit(config)
+var TweetsContainer = require(path.join(__dirname, 'lib/TweetsContainer.js'))
+var Screen = require(path.join(__dirname, 'lib/Screen.js'))
 
+var screen = Screen(),
+  tweetsContainer = TweetsContainer()
+
+screen.append(tweetsContainer)
+
+/*
 T.get('statuses/user_timeline', function (err, data, response) {
   console.log(data[0])
 })
+*/
