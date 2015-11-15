5eb0fa62d5197b5cb8d7f7cc8420a1705d1abf7e
greduan <me@greduan.com>
2015-11-15 15:48:32 -0600

    TweetsContainer has a list of tweets
    This means TweetsContainer is no longer a Box, it is now a List.
    
    Tweet is not a box, but rather just an object to keep record of an
    entire tweet.  Eventually it'd have methods of course.
    
    Screen is simpler.  Now it's just a factory for the blessed.screen
    object, it no longer adds its own methods.

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 'use strict'
 
-var path = require('path'),
-  config = require(path.join(__dirname, 'config.js')),
-  Twit = require('twit'),
-  T = new Twit(config)
+var path = require('path')
 
 var TweetsContainer = require(path.join(__dirname, 'lib/TweetsContainer.js'))
 var Screen = require(path.join(__dirname, 'lib/Screen.js'))
 
 var screen = Screen(),
-  tweetsContainer = TweetsContainer()
+  tweetsContainer = TweetsContainer(screen)
 
-screen.append(tweetsContainer)
-
-/*
-T.get('statuses/user_timeline', function (err, data, response) {
-  console.log(data[0])
-})
-*/
+screen.append(tweetsContainer.element)
+screen.render()
+tweetsContainer.getHomeTimeline()
